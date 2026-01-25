5e2b9ff1b528fed320d683b0b3dca391ae1b72d3
Aravinth <aravinth466677@gmail.com>
2026-01-25 11:03:34 +0530

    Initial deploy: coming soon site

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial",
+      background: "#111",
+      color: "#fff",
+      flexDirection: "column"
+    }}>
+      <h1>Interior Design Studio</h1>
+      <p>Website launching soon</p>
+    </div>
+  );
+}
+
+export default App;
